1147db4ee30126982e663d1eb6de4e7bcffa90e1
valeriaconte <128838224+valeriaconte@users.noreply.github.com>
2023-03-25 11:45:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+    float a=0, b=0, c=0;
+    float Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
+    cout << "inserisci tre numeri reali\n";
+    cin >> a >> b >> c;
+    Triangolo= (a*b)/2;
+    Quadrato= a*a;
+    Rettangolo= a*b;
+    Trapezio= ((a+b)*c)/2;
+    cout << Triangolo << endl;
+    cout << Quadrato << endl;
+    cout << Rettangolo << endl;
+    cout << Trapezio << endl;
+    return 0;
 }
